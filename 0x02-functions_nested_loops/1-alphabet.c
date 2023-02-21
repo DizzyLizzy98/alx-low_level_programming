ea060c029f7207b994ef3dfb7f75069f4ed35b0a
DizzyLizzy98 <elizab.a.fouche@gmail.com>
2023-02-21 17:26:04 +0200

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_alphabet-Show me all lowercase alphabet letters
+*
+*/
+
+
+void print_alphabet(void)
+	{
+		char letter;
+
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+
+		_putchar('\n');
+	}
+
